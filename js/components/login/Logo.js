73553bf3476daf27f14d8b089b05443459dd8805
harrat <naim.harrat@etudiant.univ-lille1.fr>
2018-10-13 02:07:56 +0200

    Login UI: restyle minor + assets (images)

--- js/components/login/Logo.js
+++ js/components/login/Logo.js
@@ -17,7 +17,7 @@ export default class Logo extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 3,
+    flex: 5,
     alignItems: 'center',
     justifyContent: 'center',
   },
--- js/components/login/SignupSection.js
+++ js/components/login/SignupSection.js
@@ -4,29 +4,39 @@ import Dimensions from 'Dimensions';
 import {StyleSheet, View, Text} from 'react-native';
 
 export default class SignupSection extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Create Account</Text>
-        <Text style={styles.text}>Forgot Password?</Text>
-      </View>
-    );
-  }
+
+    constructor() {
+        super();
+
+    }
+
+    handleSignIn() {
+        alert("signing in ....")
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text onPress={this.handleSignIn} style={styles.text}>Create Account</Text>
+                <Text style={styles.text}>Forgot Password?</Text>
+            </View>
+        );
+    }
 }
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    top: 65,
-    width: DEVICE_WIDTH,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  text: {
-    color: 'white',
-    backgroundColor: 'transparent',
-  },
+    container: {
+        flex: 1,
+        //top: 65,
+        width: DEVICE_WIDTH,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    text: {
+        color: 'white',
+        backgroundColor: 'transparent',
+    },
 });
