292799ead142244f69f0b08b3d5183b2f39f586a
harrat <naim.harrat@etudiant.univ-lille1.fr>
2018-10-13 20:43:17 +0200

    Login UI: SignIn V1 animation

--- js/components/login/SignupSection.js
+++ js/components/login/SignupSection.js
@@ -1,23 +1,54 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Dimensions from 'Dimensions';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Animated, Easing} from 'react-native';
+import {Actions} from "react-native-router-flux/index";
 
 export default class SignupSection extends Component {
 
     constructor() {
         super();
 
+        this.growAnimated = new Animated.Value(0);
+        this._onPress = this._onPress.bind(this);
+        this.handleSignIn = this.handleSignIn.bind(this);
     }
 
+    _onPress(){
+        this._onGrow();
+
+
+        setTimeout(() => {
+            Actions.signinScreen();
+            this.growAnimated.setValue(0);
+        }, 1000);
+    }
+
+    _onGrow() {
+        Animated.timing(this.growAnimated, {
+            toValue: 1,
+            duration: 250,
+            easing: Easing.linear,
+        }).start();
+    }
+
+
     handleSignIn() {
-        alert("signing in ....")
+        this._onPress();
+
     }
 
     render() {
+        const changeScale = this.growAnimated.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 3000],
+        });
         return (
             <View style={styles.container}>
                 <Text onPress={this.handleSignIn} style={styles.text}>Create Account</Text>
+                <Animated.View
+                    style={[styles.growinCircle, {transform: [{scale: changeScale}]}]}
+                />
                 <Text style={styles.text}>Forgot Password?</Text>
             </View>
         );
@@ -26,7 +57,9 @@ export default class SignupSection extends Component {
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
+const ANIMATION_COLOR = 'rgba(255, 255, 255, 0.5)';
 
+const MARGIN = 1;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -39,4 +72,13 @@ const styles = StyleSheet.create({
         color: 'white',
         backgroundColor: 'transparent',
     },
+    growinCircle:{
+            height: MARGIN,
+            width: MARGIN,
+            borderWidth: 1,
+            borderColor: 'white',
+            borderRadius: 100,
+            alignSelf: 'center',
+            backgroundColor: ANIMATION_COLOR
+    }
 });
