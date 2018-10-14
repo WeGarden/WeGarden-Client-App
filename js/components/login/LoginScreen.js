e1bbafce8a3a7a7715e5533930b90d5a04efaab7
harrat <naim.harrat@etudiant.univ-lille1.fr>
2018-10-14 18:26:25 +0200

    Login UI: API CALLS

--- js/components/login/LoginScreen.js
+++ js/components/login/LoginScreen.js
@@ -5,6 +5,7 @@ import Form from './Form';
 import Wallpaper from './Wallpaper';
 import ButtonSubmit from './ButtonSubmit';
 import SignupSection from './SignupSection';
+import Api from '../utils/ApiCalls';
 import {KeyboardAvoidingView} from "react-native";
 
 export default class LoginScreen extends Component {
@@ -29,8 +30,51 @@ export default class LoginScreen extends Component {
         this.setState({password: text})
     }
 
-    login(email, pass) {
-        alert('email: ' + email + ' password: ' + pass)
+    doneLogin(func) {
+        func();
+    }
+
+    login(user, pass) {
+        if (this.checkInput(user, pass)) {
+            Api.authentificateUser(user, pass, this.loginSuccess, this.loginFailed, this.connexionFailed);
+        } else {
+            alert({title: "Error", message: "Please enter your login Username and Password"});
+        }
+    }
+
+    /**
+     * true if inputs are correct, false otherwise
+     * @param user the entered username
+     * @param pass the entered password
+     * @returns true if inputs are correct, false otherwise
+     */
+    checkInput(user, pass) {
+        return user && pass;
+    }
+
+    /**
+     * login success callback function
+     * @param res json of the api response {'accessToken':?}
+     */
+    loginSuccess(res) {
+//login
+        alert("Welcome back!");
+    }
+
+
+    /**
+     * connexion failed callback function
+     * @param error json of the api error
+     */
+    connexionFailed(error) {
+        alert("Connexion failed" + JSON.stringify(error))
+    }
+
+    /**
+     * login failed callback function
+     */
+    loginFailed() {
+        alert("Wrong Username/Password");
     }
 
     render() {
@@ -38,7 +82,7 @@ export default class LoginScreen extends Component {
             <Wallpaper>
                 <KeyboardAvoidingView behavior='padding' style={{flex: 1}}>
                     <Logo/>
-                    <Form handleUsername={this.handleUsername} handlePassword={this.handlePassword}/>
+                    {<Form handleUsername={this.handleUsername} handlePassword={this.handlePassword}/>}
                     <ButtonSubmit handleLogin={() => {
                         this.login(this.state.username, this.state.password)
                     }}/>
@@ -47,6 +91,7 @@ export default class LoginScreen extends Component {
                         //signIngUrl={/*TODO*/} forgotPassordUrl={/*TODO*/}
                     />
                 </KeyboardAvoidingView>
+
             </Wallpaper>
         );
     }
--- js/components/login/Logo.js
+++ js/components/login/Logo.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {StyleSheet, View, Text, Image, KeyboardAvoidingView} from 'react-native';
 
 import logoImg from '../../../assets/images/logo.png';
 
@@ -8,7 +8,9 @@ export default class Logo extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <KeyboardAvoidingView behavior={'height'}>
         <Image source={logoImg} style={styles.image} />
+        </KeyboardAvoidingView>
         <Text style={styles.text}>We Garden</Text>
       </View>
     );
@@ -22,13 +24,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image: {
-    width: 80,
-    height: 80,
+    width: 200,
+    height: 200,
   },
   text: {
+    fontSize:50,
     color: 'white',
     fontWeight: 'bold',
-    backgroundColor: 'transparent',
-    marginTop: 20,
+    backgroundColor: 'green',
+    marginTop: 10,
+      padding: 10,
+      borderRadius:100
+
   },
 });
