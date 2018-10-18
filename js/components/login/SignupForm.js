2e0e2c4eaf0c96fdeedf1edc57b4ea4f46f8dad5
Harrat Naim <harrat@b10p23.fil.univ-lille1.fr>
2018-10-18 19:48:06 +0200

    Signup screen

---
+++ js/components/login/SignupForm.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Dimensions from 'Dimensions';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+import UserInput from './UserInput';
+
+import usernameImg from '../../../assets/images/username.png';
+import passwordImg from '../../../assets/images/password.png';
+import eyeImg from '../../../assets/images/eye_black.png';
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPass: true,
+      press: false,
+    };
+    this.showPass = this.showPass.bind(this);
+  }
+
+  showPass() {
+    this.state.press === false
+      ? this.setState({ showPass: false, press: true })
+      : this.setState({ showPass: true, press: false });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <UserInput
+          source={usernameImg}
+          placeholder="Username"
+          autoCapitalize={'none'}
+          returnKeyType={'done'}
+          autoCorrect={false}
+          handler={this.props.handleUsername}
+        />
+        <UserInput
+          source={usernameImg} //TODO change to email Image
+          placeholder="Email"
+          autoCapitalize={'none'}
+          returnKeyType={'done'}
+          autoCorrect={false}
+          handler={this.props.handleEmail}
+        />
+        <UserInput
+          source={passwordImg}
+          secureTextEntry={this.state.showPass}
+          placeholder="Password"
+          returnKeyType={'done'}
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          handler={this.props.handlePassword}
+        />
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.btnEye}
+          onPress={this.showPass}>
+          <Image source={eyeImg} style={styles.iconEye} />
+        </TouchableOpacity>
+        <UserInput
+          source={passwordImg}
+          secureTextEntry={this.state.showPass}
+          placeholder="Confirm Password"
+          returnKeyType={'done'}
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          handler={this.props.handleConfirmPassword}
+        />
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.btnEye}
+          onPress={this.showPass}>
+          <Image source={eyeImg} style={styles.iconEye} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
+
+
+Form.propTypes = {
+  handlePassword: PropTypes.func.isRequired,
+  handleUsername: PropTypes.func.isRequired,
+  handleConfirmPassword: PropTypes.func.isRequired,
+  handleEmail: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    height: 100,
+    alignItems: 'center'
+  },
+  btnEye: {
+    position: 'absolute',
+    top: 55,
+    right: 28,
+  },
+  iconEye: {
+    width: 25,
+    height: 25,
+    tintColor: 'rgba(0,0,0,0.2)',
+  },
+});
---
+++ js/components/login/SignupScreen.js
@@ -0,0 +1,185 @@
+import React, { Component } from 'react';
+import Logo from './Logo';
+import Wallpaper from './Wallpaper';
+import { AsyncStorage } from "react-native";
+import ButtonSubmit from './ButtonSubmit';
+import Api from '../utils/ApiCalls';
+import { KeyboardAvoidingView, TouchableOpacity } from "react-native";
+import { Keyboard } from 'react-native';
+import { Actions } from "react-native-router-flux/index";
+
+
+const MIN_PASS_LENGTH = 6;
+export default class SignupScreen extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+            confirmPassowrd: ''
+        };
+
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleConfirmPassword = this.handleConfirmPassword.bind(this);
+        this.handleUsername = this.handleUsername.bind(this);
+        this.signupAction = this.signupAction.bind(this);
+    }
+
+    /**
+     * username setter
+     * 
+     * @param {String} text entered username
+     */
+    handleUsername(text) {
+        this.setState({ username: text })
+    }
+
+
+    /**
+     * email setter
+     * 
+     * @param {String} text entered username
+     */
+    handleEmail(text) {
+        this.setState({ email: text })
+    }
+
+    /**
+     * password setter
+     * 
+     * @param {String} text entered password
+     */
+    handlePassword(text) {
+        this.setState({ password: text })
+    }
+
+    /**
+     * confirm password setter
+     * 
+     * @param {String} text entered password
+     */
+    handleConfirmPassword(text) {
+        this.setState({ confirmPassword: text })
+    }
+
+    /**
+     * login function, calls checkinput then tries to authentificate user, 
+     * 
+     * @param {function} doneLoading 
+     */
+    signupAction(doneLoading) {
+        let user = this.state.username;
+        let pass = this.state.password;
+
+        if (this.checkInput(user, pass)) {
+            Api.signup(user, pass, this.signinSuccess, 
+                this.alreadyExists, this.connexionFailed).then(() => {
+                    doneLoading();
+                    Actions.pop();
+                });
+        } else {
+            alert("Please enter your login Username and Password");
+            doneLoading();
+        }
+    }
+
+
+    static checkPasswordLength() {
+        return this.state.password.length >= MIN_PASS_LENGTH;
+    }
+
+    static checkPasswordConfirm() {
+        return this.state.password === this.state.confirmPassowrd;
+    }
+
+
+
+    /**
+     * true if inputs are correct, false otherwise
+     * @param user the username
+     * @param pass the password
+     * @returns true if inputs are correct, false otherwise
+     */
+    static checkInput(user, pass) {
+        if (this.checkPasswordLength()) {
+            alert("Password must have at least 6 caracters");
+        } if (this.checkPasswordConfirm()) {
+            alert("Password's confirmation doesn't match");
+        }
+
+
+        return user && pass;
+    }
+
+    /**
+     * login api success callback function
+     * @param res json of the api response {'accessToken':?}
+     */
+    static signinSuccess(res) {
+        //login
+        alert(res.accessToken);
+        AsyncStorage.setItem("userToken", res.accessToken);
+        Actions.signinScreen() // TODO change it to home page
+    }
+
+
+    /**
+     * connexion failed callback function
+     * @param error json of the api error
+     */
+    static connexionFailed(error) {
+        alert("Connexion failed" + JSON.stringify(error));
+    }
+
+    /**
+     * login failed callback function
+     */
+    static loginFailed() {
+        alert("Wrong Username/Password");
+    }
+
+    render() {
+
+        return (
+            <TouchableOpacity activeOpacity={1} onPress={Keyboard.dismiss}>
+                <Wallpaper>
+                    <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
+                        <Logo />
+
+                        <SignupForm handleEmail={this.handleEmail}
+                            handlePassword={this.handlePassword}
+                            handleConfirmPassword={this.handleConfirmPassword}
+                            handleUsername = {this.handleUsername}
+                        />
+                        <ButtonSubmit handleSubmit={this.signupAction} />
+
+                        <SignupSection
+                        //signIngUrl={/*TODO*/} forgotPassordUrl={/*TODO*/}
+                        />
+                    </KeyboardAvoidingView>
+                </Wallpaper>
+            </TouchableOpacity>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    containerForm: {
+        //flex: 1,
+        height: 100,
+        alignItems: 'center'
+    },
+    btnEye: {
+        position: 'absolute',
+        top: 55,
+        right: 28,
+    },
+    iconEye: {
+        width: 25,
+        height: 25,
+        tintColor: 'rgba(0,0,0,0.2)',
+    },
+});
